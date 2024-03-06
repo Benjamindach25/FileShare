e180380de3df644213664b2eecdee3e21f638bb2
Benjamin Dace <149401503+Benjamindach25@users.noreply.github.com>
2024-03-07 01:44:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6949902115"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://maya:maya@maya.kweokda.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "mayava")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://maya:maya@maya.mm1xscx.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "maya")
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "2"))
